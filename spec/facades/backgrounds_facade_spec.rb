dea8d20a3a49af5610416f4756aed3fc32d00077
Croix Kimmel <croixkimmel@gmail.com>
2022-03-06 11:08:18 -0700

    Correct facade test - fix name discrepancy

--- spec/facades/backgrounds_facade_spec.rb
+++ spec/facades/backgrounds_facade_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe BackgroundsFacade do
   it 'can return hash with background information' do
     json = BackgroundsFacade.get_image('denver,co')
-    expect(lat_long).to be_a(Hash)
+    expect(json).to be_a(Hash)
   end
 end
