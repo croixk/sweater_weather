e25656163abd1725c83256fc9c15e456b02b63f9
Croix Kimmel <croixkimmel@gmail.com>
2022-03-08 14:53:26 -0700

    Update code to pass updated login tests

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,14 +2,18 @@ class Api::V1::SessionsController < ApplicationController
 
   def login
     user = User.find_by(email: params[:email])
-    if user.authenticate(params[:password])
-      session[:user_id] = user.id
-      session[:email] = user.email
-      session[:api_key] = user.api_key
+    if user != nil
+      if user.authenticate(params[:password])
+        session[:user_id] = user.id
+        session[:email] = user.email
+        session[:api_key] = user.api_key
 
-      render json: SessionsSerializer.session_return(session)
+        render json: SessionsSerializer.session_return(session)
+      else
+        render json: { data: { error: 'Incorrect login credentials'}}
+      end
     else
-
+      render json: { data: { error: 'Incorrect login credentials'}}
     end
   end
 
