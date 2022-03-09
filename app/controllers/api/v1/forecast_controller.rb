3f4593ed0c73989aac11b10a12340037813bc9e3
Croix Kimmel <croixkimmel@gmail.com>
2022-03-09 09:03:05 -0700

    Last updates

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ForecastController < ApplicationController
+  
   def show
     coordinates = MapFacade.lat_long(params[:location])
     weather = WeatherFacade.get_weather(coordinates[:lat], coordinates[:lng])
